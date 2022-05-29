bf414e8966e83a473d52fb1dec1460da82d40979
Liu Ye <liuye5@live.com>
2022-05-29 14:19:27 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -2,7 +2,11 @@
 
 echo "I'm running ${COMPOSE_PROJECT_NAME}"
 
-wget https://github.com/ProjectOpenSea/opensea-creatures.git
+wget https://github.com/crytic/slither/archive/refs/heads/master.zip
+
+unzip master.zip
+
+mv master opensea-creatures 
 
 cd opensea-creatures 
 
