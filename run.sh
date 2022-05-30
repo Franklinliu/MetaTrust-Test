ce548a64c832c755c1d55aac7bfa729e2a6826d1
Liu Ye <liuye5@live.com>
2022-05-30 10:14:43 +0800

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,12 +4,28 @@ echo "I'm running ${COMPOSE_PROJECT_NAME}"
 
 apt-get install unzip
 
+wget https://github.com/OpenZeppelin/openzeppelin-contracts/archive/refs/heads/master.zip
+
+unzip master.zip
+
+rm master.zip
+
+cd openzeppelin-contracts-master
+
+npm install
+
+slither . --json /usr/share/slither-result/openzeppelin-contracts.json
+
 wget https://github.com/ProjectOpenSea/opensea-creatures/archive/refs/heads/master.zip
 
 unzip master.zip
+
+rm master.zip
 
 cd opensea-creatures-master 
 
 npm install
 
 slither . --json /usr/share/slither-result/opensea-creatures.json
+
+
